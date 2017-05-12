7b4a370a2e30d6fcde65a4a63fe27416f9b8b057
Stacy Dillahey <stacy.dillahey@gmail.com>
2017-05-12 11:51:33 -0700

    initial commit

---
+++ controllers/foodTruckController.js
@@ -0,0 +1,28 @@
+var foodTruckController = {
+  index: function(req, res, next){
+    res.send('What up');
+  },
+  new: function(req, res, next){
+
+  },
+  create: function(req, res, next){
+
+  },
+  show: function(req, res, next){
+
+  },
+  delete: function(req, res, next){
+
+  },
+  map: function(req, res, next){
+
+  },
+  edit: function(req, res, next){
+
+  },
+  update: function(req, res, next){
+
+  }
+};
+
+module.exports = foodTruckController;
---
+++ routes/foodtruck.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const foodTruckController = require('../controllers/foodTruckController');
+
+/* RESTful route definitions */
+router.get('/', foodTruckController.map);
+router.get('/favorites', foodTruckController.index);
+router.get('/favorites/new', foodTruckController.new);
+router.post('/favorites', foodTruckController.create);
+router.get('/favorites/:id', foodTruckController.show);
+router.get('/favorites/:id/edit', foodTruckController.edit);
+router.put('/favorites/:id', foodTruckController.update);
+router.delete('/favorites', foodTruckController.delete);
+
+module.exports = router;
